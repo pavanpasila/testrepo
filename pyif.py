76c06b2136a94ba4cf475cbbba2fb4ce32d30f59
maha2620 <maheedevops@gmail.com>
2020-04-15 10:41:04 +0000

    python file added

---
+++ pyif.py
@@ -0,0 +1,24 @@
+x=10
+if x % 2 == 0:
+    print(x, " is even number")
+else:
+    print(x, " is odd number")
+
+print(type(x))
+print(id(x))
+print(x)
+a=1000
+b=34.56
+c=3+4j
+d=True
+e='python'
+print(type(a))
+print(a)
+print(type(b))
+print(b)
+print(type(c))
+print(c)
+print(type(d))
+print(d)
+print(type(e))
+print(e)
